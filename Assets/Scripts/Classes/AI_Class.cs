144473f9de20b8985971a5e555a1c56c0736b881
ertugrul013 <ertugrul013@hotmail.com>
2018-03-28 13:45:44 +0200

    trying to solve merge conflicts

--- Assets/Scripts/Classes/AI_Class.cs
+++ Assets/Scripts/Classes/AI_Class.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class AI_Class{
 
 	public enum Type{FatKid, FastKid, NormalKid};
-	public Type MyType;
+	public Type TypeKid;
+	public bool isChaser;
 	public float stamina;
 	public float reactionSpeed;
 	public float fov;
@@ -14,22 +15,24 @@ public class AI_Class{
 	public AI_Class()
 	{
 		int t = Random.Range(0,3);
-
 		switch (t)
 		{
 			case 0:
+				TypeKid = Type.FatKid;
 				stamina = 30f;
 				reactionSpeed = 4f;
 				fov = 170f;
 			break;
 
 			case 1:
+				TypeKid = Type.FastKid;
 				stamina = 80f;
 				reactionSpeed = 1f;
 				fov = 130f;
 			break;
 
 			case 2:
+				TypeKid = Type.NormalKid;
 				stamina = 50f;
 				reactionSpeed = 2f;
 				fov = 150f;
