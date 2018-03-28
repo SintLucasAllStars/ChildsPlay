c271a776395e5b97714bf619d7bd5d121766d479
ertugrul013 <ertugrul013@hotmail.com>
2018-03-28 13:33:39 +0200

    finished work on the iniatel class setup

---
+++ Assets/Prefabs/Classes/AI_Class.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Class{
+
+	public enum Type{FatKid, FastKid, NormalKid};
+	public Type MyType;
+	public float stamina;
+	public float reactionSpeed;
+	public float fov;
+
+
+	public AI_Class()
+	{
+		int t = Random.Range(0,3);
+
+		switch (t)
+		{
+			case 0:
+				stamina = 30f;
+				reactionSpeed = 4f;
+				fov = 170f;
+			break;
+
+			case 1:
+				stamina = 80f;
+				reactionSpeed = 1f;
+				fov = 130f;
+			break;
+
+			case 2:
+				stamina = 50f;
+				reactionSpeed = 2f;
+				fov = 150f;
+			break;
+
+			default:
+				Debug.Log("POP GOES THE WEAZEL");
+			break;
+		}
+	}
+}
