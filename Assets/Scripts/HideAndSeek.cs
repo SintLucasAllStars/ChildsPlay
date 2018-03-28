9becbdfa50e753b09462182c52ed5353b1d17a75
Marcelle Fransen <marcellefransen@hotmail.com>
2018-03-28 14:18:17 +0200

    win/lose
    you can win and lose

--- Assets/Scripts/HideAndSeek.cs
+++ Assets/Scripts/HideAndSeek.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HideAndSeek : MonoBehaviour {
+	public GameObject[] hidePlaces = new GameObject[4];
+	GameObject buffer;
 
 	// Use this for initialization
 	void Start (){
@@ -16,17 +18,23 @@ public class HideAndSeek : MonoBehaviour {
 			RaycastHit hit;
 			if (Physics.Raycast (ray, out hit)) {
 				if (hit.transform.name == "Hide 1") {
-					Debug.Log ("jeej");
+					buffer = hidePlaces [0];
 				}
 				if (hit.transform.name == "Hide 2") {
-					Debug.Log ("jeej");
+					buffer = hidePlaces [1];
 				}
 				if (hit.transform.name == "Hide 3") {
-					Debug.Log ("jeej");
+					buffer = hidePlaces [2];
 				}
 				if (hit.transform.name == "Hide 4") {
-					Debug.Log ("jeej");
+					buffer = hidePlaces [3];
 				}
+			}
+			if (hidePlaces[Random.Range (0, 4)] == buffer){
+				Debug.Log ("you lost");
+			}
+			else {
+				Debug.Log ("you win");
 			}
 		}
 	}
