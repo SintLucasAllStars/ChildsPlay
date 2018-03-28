63507e3c1c5bcb6c37045243b238a221891ea281
Tijn van den Broek <TijnWorks@users.noreply.github.com>
2018-03-28 10:49:14 +0200

    First push for the Hiding AI & Game Manager.
    Added a basic character for the hiding AI and started on the script, also added an empty game manager + script so it's referencable.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Placeholder : MonoBehaviour {
+public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
