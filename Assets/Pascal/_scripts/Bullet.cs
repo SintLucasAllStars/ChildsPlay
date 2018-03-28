a690b07380e25a7a77df8912d808a8c5e099d805
HoekigeHeer <ploxerdongaming@gmail.com>
2018-03-28 13:26:37 +0200

    Added a working gun that shoots bullets

---
+++ Assets/Pascal/_scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	float downwardsGravity;
+	float gravityReducer;
+	// Use this for initialization
+	void Start () {
+		downwardsGravity = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		downwardsGravity = Mathf.Pow (downwardsGravity, downwardsGravity);
+		transform.position = transform.position + Vector3.forward;
+}
+}
---
+++ Assets/Pascal/_scripts/Shoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+	public GameObject bullet;
+	public GameObject bulletSpawnPoint;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Mouse0)) {
+			ShootBullet ();
+		}
+	}
+
+	void ShootBullet(){
+		Instantiate (bullet, bulletSpawnPoint.transform.position, bulletSpawnPoint.transform.rotation);
+	}
+}
