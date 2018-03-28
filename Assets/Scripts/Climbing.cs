eb0647b1c546217c306578427b053ba8a7fd6dd5
Kars van den Eijnde <aofbliz@hotmail.com>
2018-03-28 23:33:46 +0200

    working on climbing animation
    made a walking animation
    
    reworked climbing animation
    
    merged climbing script with PlayerBehavior script
    
    added a switch for animation states (currently doing nothing).

--- Assets/Scripts/Climbing.cs
+++ Assets/Scripts/Climbing.cs
@@ -8,28 +8,36 @@ public class Climbing : MonoBehaviour {
     public Animator anim;
     public Camera regularCam;
     public Camera parcourCam;
+    public Animation climbAnim;
+    bool isClimbing = false;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (canClimb && Input.GetKeyDown(KeyCode.Space))
+        if (canClimb && Input.GetKeyDown(KeyCode.Space) && !isClimbing)
         {
+            isClimbing = true;
             regularCam.depth = 0;
             parcourCam.depth = 1;
             rb.isKinematic = true;
-            anim.SetTrigger("Climbup");
+            anim.SetBool("Climbup",true);
             StartCoroutine(afterClimb());
         }
 	}
     IEnumerator afterClimb(){
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.3f);
+        anim.SetBool("Climbup" ,false);
+        while (anim.GetCurrentAnimatorStateInfo(0).IsName("Climb"))
+        {
+            yield return null;
+        }
         regularCam.depth = 1;
         parcourCam.depth = 0;
         rb.isKinematic = false;
+        isClimbing = false;
         transform.position = parcourCam.transform.position;
     }
     void OnTriggerEnter (Collider other){
--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -3,29 +3,75 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour {
-
-
-
-
+    enum AniState{Idle,Walk,Climb}
+    AniState CurAni;
+    bool isClimbing = false;
+    public bool canClimb;
 	bool isTagger = true;
 	Rigidbody rb;
-	public float speed = 6f;
+	public float speed;
 	float mouseSense= 3f;
+    public Animator anim;
 
 	void Awake () {
 		Cursor.lockState = CursorLockMode.Locked;
-
 		rb = GetComponent<Rigidbody> ();
 	}
+    void update(){
 
+        switch (CurAni)
+        {
+            case (AniState.Idle):
+                anim.SetBool("Walking", false);
+                anim.SetBool("climbup",false);
+                break;
+            case (AniState.Walk):
+                anim.SetBool("Walking", true);
+                break;
+            case (AniState.Climb):
+                anim.SetBool("climbup", true);
+                break;
+                
+        }
+
+        if (canClimb && Input.GetKeyDown(KeyCode.Space) && !isClimbing)
+            Debug.Log("thou shal climb");
+            isClimbing = true;
+            rb.isKinematic = true;
+            anim.SetBool("Climbup", true);
+            StartCoroutine(afterClimb());
+        }
+
+
+//    if(Input.GetKeyDown(KeyCode.W))
+//        Debug.Log("work pls");
+//        anim.SetBool("Walking",true);
+    
+    IEnumerator afterClimb(){
+        yield return new WaitForSeconds(0.3f);
+        anim.SetBool("Climbup", false);
+        while (anim.GetCurrentAnimatorStateInfo(0).IsName("Climb2"))
+        {
+            yield return null;
+        }
+        rb.isKinematic = false;
+        isClimbing = false;
+    }
 	void FixedUpdate () {
 		float horMove = Input.GetAxis("Horizontal");
 		float verMove = Input.GetAxis("Vertical");
-		//rb.velocity = new Vector3(horMove, 0f,verMove).normalized   * speed;
 
-		rb.velocity = transform.forward * verMove + transform.right * horMove;
+        if(horMove != 0 || verMove != 0)
+        rb.velocity = (transform.forward * verMove + transform.right * horMove)*speed;
+        
 
 		transform.Rotate (0, Input.GetAxis ("Mouse X") * mouseSense, 0f);
 		transform.GetChild (0).Rotate (-Input.GetAxis("Mouse Y") * mouseSense,0f,0f);
 	}
+    void OnTriggerEnter (Collider other){
+        canClimb = true;
+    }
+    void OnTriggerExit (Collider other){
+        canClimb = false;
+    }
 }
