df4a049bd2e687c92b0bb98fca2c2113188a08fb
Juul Cox <Juulcox00@Gmail.com>
2018-03-28 10:36:22 +0200

    add fork

---
+++ Fork and pull/Assets/AIBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIBehaviour : MonoBehaviour {
+	public enum Mode {seek, hide, chase};
+
+	NavMeshAgent nav;
+	public Transform target;
+	public Mode mode;
+
+
+	void Start () {
+		nav = GetComponent<NavMeshAgent> ();
+		SetMode (Mode.seek);
+	}
+
+	void Update () {
+
+		switch (mode) {
+
+		case Mode.seek:
+			
+		break;
+		
+		case Mode.hide:
+
+		break;
+		
+		case Mode.chase:
+
+		break;
+
+		}
+		
+	}
+
+
+
+	void SetMode (Mode m){
+		mode = m;
+	}
+}
