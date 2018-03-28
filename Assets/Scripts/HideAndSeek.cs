c82f607bbe625d9ae1af988166dfb49a52905529
Marcelle Fransen <marcellefransen@hotmail.com>
2018-03-28 13:46:05 +0200

    click on hide places
    you can now click on the hide places and get a respond in the console

--- Assets/Scripts/HideAndSeek.cs
+++ Assets/Scripts/HideAndSeek.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class HideAndSeek : MonoBehaviour {
 
 	// Use this for initialization
-	void Start ()
-    {
-		//hallo
+	void Start (){
+		
 	}
 	
 	// Update is called once per frame
-	void Update ()
-    {
-		
+	void Update (){
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) {
+				if (hit.transform.name == "Hide 1") {
+					Debug.Log ("jeej");
+				}
+				if (hit.transform.name == "Hide 2") {
+					Debug.Log ("jeej");
+				}
+				if (hit.transform.name == "Hide 3") {
+					Debug.Log ("jeej");
+				}
+				if (hit.transform.name == "Hide 4") {
+					Debug.Log ("jeej");
+				}
+			}
+		}
 	}
 }
