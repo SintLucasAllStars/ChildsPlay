c09c4db7891f15cc487e63698365255effbab5a9
Tijn van den Broek <TijnWorks@users.noreply.github.com>
2018-03-28 12:20:36 +0200

    Changed the scene, made the hide AI run away.
    It still needs more functions and a proper hiding state, but we're working on it.

--- Assets/Scripts/HiderBehaviour.cs
+++ Assets/Scripts/HiderBehaviour.cs
@@ -5,6 +5,9 @@ using UnityEngine.AI;
 
 public class HiderBehaviour : MonoBehaviour {
 
+	public enum AIstate {moving,hiding,captured};
+	public AIstate currentState;
+
 	public Vector3 startPosition;
 	public Vector3 hidingPosition;
 
@@ -19,16 +22,47 @@ public class HiderBehaviour : MonoBehaviour {
 
 		//seeker = GameObject.FindGameObjectWithTag ("Seeker");
 		gm = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameManager> ();
+		nma = GetComponent<NavMeshAgent> ();
+
 		startPosition = transform.position;
 		hidingPosition = new Vector3 (Random.Range (-mapSize/2, mapSize/2), 1, Random.Range (-mapSize/2, mapSize/2));
-
+		nma.SetDestination (hidingPosition);
+		currentState = AIstate.moving;
 
 	}
 
 	void Update () {
 
+		switch (currentState) {
+		case AIstate.moving:
+			Moving ();
+			break;
+		case AIstate.hiding:
+			Hiding ();
+			break;
+		case AIstate.captured:
+			Captured ();
+			break;
+		default:
+			Hiding ();
+			break;
+		}
 
+	}
 
+	public void Moving(){
+		nma.SetDestination (hidingPosition);
+		if (nma.remainingDistance < 0.5f) {
+			currentState = AIstate.hiding;
+		}
+	}
+
+	public void Hiding(){
+		
+	}
+
+	public void Captured(){
+		nma.SetDestination (hidingPosition);
 	}
 
 }
