12757dd4372248e82fac781df58c97a9bb5e0e5b
deMachtige <borisramakers99@outlook.com>
2018-03-28 11:06:24 +0200

    Added doors and a basic player

---
+++ Fork and pull/Assets/PlayerLook.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour {
+
+	public float mousSensitivity;
+	public Transform playerBoddy;
+
+	float xAxisClamp= 0f;
+
+	void Awake(){
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F)) {
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+
+		float mouseX = Input.GetAxis ("Mouse X");
+		float mouseY = Input.GetAxis ("Mouse Y");
+
+		float rotAmountX = mouseX * mousSensitivity;
+		float rotAmountY = mouseY * mousSensitivity;
+
+		xAxisClamp -= rotAmountY;
+
+		Vector3 targetRotCam = transform.rotation.eulerAngles;
+		Vector3 targetRotBody = playerBoddy.rotation.eulerAngles;
+
+		targetRotCam.x -= rotAmountY;
+		targetRotCam.z = 0;
+		targetRotBody.y += rotAmountX;
+
+		if (xAxisClamp > 90) {
+			xAxisClamp = 90;
+			targetRotCam.x = 90;
+		}
+		else if (xAxisClamp < -90) {
+			xAxisClamp = -90;
+			targetRotCam.x = 270;
+		}
+
+		transform.rotation = Quaternion.Euler (targetRotCam);
+		playerBoddy.rotation =  Quaternion.Euler (targetRotBody);
+
+	}
+
+
+}
---
+++ Fork and pull/Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public float forwardSpeed;
+	public float sidewaySpeed;
+	Rigidbody rb;
+	float currentForSpeed;
+	float currentSideSpeed;
+	public float sprintMultiplier;
+	float currentSprintMulti;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			currentSprintMulti = sprintMultiplier;
+		} else {
+			currentSprintMulti = 1f;
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			currentForSpeed = forwardSpeed*currentSprintMulti;
+		} else if (Input.GetKey (KeyCode.S)) {
+			currentForSpeed = -forwardSpeed;
+		} else {
+			currentForSpeed = 0f;
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			currentSideSpeed = sidewaySpeed*currentSprintMulti;
+		} else if (Input.GetKey (KeyCode.A)) {
+			currentSideSpeed = -sidewaySpeed*currentSprintMulti;
+		} else {
+			currentSideSpeed = 0f;
+		}
+
+		
+		rb.velocity = transform.forward * currentForSpeed + transform.right * currentSideSpeed;
+		}
+	}
+
