5494a2dbaf433854199344ec7e22bc0384a78a11
Gijze <virtualgamedesk@gmail.com>
2018-03-28 13:44:07 +0200

    Made a terrain generator
    i have made a terrain generator using the scale and 2 dimensional floats.

---
+++ Assets/TerrainGenerator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour {
+	public int width = 256;
+	public int height = 256;
+
+	public int depth = 30;
+	public float scale = 3f;
+
+
+	void Start()
+	{
+		depth = 30;
+		scale = 3f;
+		Terrain terrain = GetComponent<Terrain>();
+		terrain.terrainData = GenerateTerrain (terrain.terrainData);
+	}
+
+	TerrainData GenerateTerrain(TerrainData terrainData) {
+		terrainData.heightmapResolution = width + 1;
+		terrainData.size = new Vector3 (width, depth, height);
+		terrainData.SetHeights (0, 0, GenerateHeights());
+		return terrainData;
+	}
+	float[,] GenerateHeights ()
+	{
+		float[,] heights = new float[width, height];
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y< height; y++)
+			{
+				heights [x, y] = CalculateHeight (x, y);
+			}
+		}
+		return heights;
+	}
+	float CalculateHeight (int x, int y)
+	{
+		float xCoord = (float)x / width * scale;
+		float yCoord = (float)y / height * scale;
+
+		return Mathf.PerlinNoise (xCoord, yCoord);
+	}
+}
