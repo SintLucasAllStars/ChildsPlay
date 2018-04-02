cf51eaaa69e264a42854ee2ebc0fd75d41de2cba
Kars van den Eijnde <aofbliz@hotmail.com>
2018-04-02 14:11:53 +0200

    changed playerBehaviour

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour {
     enum AniState{Idle,Walk,Climb}
-    AniState CurAni;
+    AniState curAni;
     bool isClimbing = false;
     public bool canClimb;
 	bool isTagger = true;
@@ -17,12 +17,11 @@ public class PlayerBehaviour : MonoBehaviour {
 		rb = GetComponent<Rigidbody> ();
 
 	}
-    void start(){
-        CurAni = AniState.Idle;
+    void Start(){
+        curAni = AniState.Idle;
     }
-    void update(){
-        Debug.Log(CurAni);
-        switch (CurAni)
+    void Update(){
+        switch (curAni)
         {
             case (AniState.Idle):
                 anim.SetBool("Walking",false);
@@ -30,21 +29,28 @@ public class PlayerBehaviour : MonoBehaviour {
                 break;
             case (AniState.Walk):
                 anim.SetBool("Walking", true);
+                anim.SetBool("climbup",false);
                 break;
             case (AniState.Climb):
                 anim.SetBool("climbup", true);
+                anim.SetBool("Walking",false);
                 break;      
         }
         if (canClimb && Input.GetKeyDown(KeyCode.Space) && !isClimbing)
-            Debug.Log("thou shall climb");
+//            Debug.Log("thou shall climb");
+        {
             isClimbing = true;
             rb.isKinematic = true;
-            anim.SetBool("Climbup", true);
+            curAni = AniState.Climb;
             StartCoroutine(afterClimb());
+            }
+        if (Input.anyKey == false)
+            anim.SetBool("Walking",false);
         }
     IEnumerator afterClimb(){
         yield return new WaitForSeconds(0.3f);
-        anim.SetBool("Climbup", false);
+//        anim.SetBool("Climbup", false);
+        curAni = AniState.Walk;
         while (anim.GetCurrentAnimatorStateInfo(0).IsName("Climb2"))
         {
             yield return null;
@@ -57,7 +63,7 @@ public class PlayerBehaviour : MonoBehaviour {
 		float verMove = Input.GetAxis("Vertical");
 
         if (horMove != 0 || verMove != 0)
-            CurAni = AniState.Walk;
+            curAni = AniState.Walk;
         rb.velocity = (transform.forward * verMove + transform.right * horMove)*speed;
 		rb.velocity = (transform.forward * verMove + transform.right * horMove) * speed;
 
