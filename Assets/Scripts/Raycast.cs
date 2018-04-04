5968eda62b59c30643862e8b77a04d78d93abd7b
craftercis <c.donkers@sintlucasedu.nl>
2018-04-04 11:14:15 +0200

    Raycast added

---
+++ Assets/Scripts/Raycast.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Raycast : MonoBehaviour {
+
+    public GameObject[] hidePlaces = new GameObject[4];
+    public Animation[] selected = new Animation[4];
+
+    public float range = 100f;
+    public Camera fpsCam;
+
+    public Image chosenBlockimage;
+    public Sprite chosenBlock;
+
+    RaycastHit hit;
+
+    // Use this for initialization
+    void Start ()
+    {
+        chosenBlockimage = GetComponent<Image>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 100;
+        Debug.DrawRay(transform.position, forward, Color.green);
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range) && Input.GetButtonDown("Fire1"))
+        {
+            for (int i = 0; i < hidePlaces.Length; i++)
+            {
+                if (hit.collider.gameObject == hidePlaces[i].gameObject)
+                {
+                    Debug.Log("This is " + hidePlaces[i] + " bring it back to the spawn.");
+                    selected[i].Play("Selected");
+                    chosenBlockimage.sprite = chosenBlock;
+                }
+            }
+        }
+    }
+}
