d657bce8e4b0b87ef33866143eaf01d63c1a3807
Max Neijndorff <m.neijndorff1999@gmail.com>
2018-04-04 13:44:34 +0100

    the third person controller works and doesn't go through walls also making states for him so he can sneek

--- Assets/Scripts/ThirdPersonScript.cs
+++ Assets/Scripts/ThirdPersonScript.cs
@@ -4,24 +4,54 @@ using UnityEngine;
 
 public class ThirdPersonScript : MonoBehaviour {
 
+    enum State { Walking, sneeking, running}
+    State state;
     public int speed;
     public int turningSpeed;
+    public float conspicuousness = 1;
+    Rigidbody rb;   
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        switch (state)
+        {
+            case State.Walking:
+                speed = 8;
+                break;
+            case State.sneeking:
+                conspicuousness = 0.5f;
+                speed = 4;
+                break;
+            case State.running:
+                speed = 10;
+                break;
+            default:
+                break;
+        }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(0, 0, speed * Time.deltaTime);
+            rb.velocity = transform.forward * speed;
+        } else if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = transform.forward * -speed;
+        }
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            state = State.sneeking;
+        }
+        else
+        {
+            state = State.Walking;
+            rb.velocity = Vector3.zero;
         }
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(0, 0, -speed * Time.deltaTime);
-        }
+
 
         if (Input.GetKey(KeyCode.A))
         {
