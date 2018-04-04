f1584255a85629c3991cd84d0b716594dd04001d
HoekigeHeer <ploxerdongaming@gmail.com>
2018-04-04 12:22:01 +0200

    Door works
    Door opens/closes with the F key

---
+++ Assets/Pascal/_scripts/Door.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+	Vector3 startPosition;
+	Vector3 endPosition;
+	float timeToMove;
+	float timeToExecute;
+	float startTime;
+	float endPositionZValue;
+	bool wasOpen;
+	enum DoorStates{Open, Closed, Moving }
+	DoorStates doorState;
+
+
+
+	// Use this for initialization
+	void Start () {
+		doorState = DoorStates.Open;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		switch (doorState) {
+		case (DoorStates.Closed):
+			endPositionZValue = -11.5f;
+			wasOpen = false;
+			break;
+
+		case (DoorStates.Open):
+			endPositionZValue = 11.5f;
+			wasOpen = true;
+			break;
+
+		case (DoorStates.Moving):
+			break;
+		}
+			
+
+		if (Input.GetKey (KeyCode.F) && (doorState != DoorStates.Moving) ) {
+			startPosition = transform.position;
+			timeToMove = 2f;
+			timeToExecute = timeToMove;
+			endPosition = new Vector3 (transform.position.x, transform.position.y, transform.position.z + endPositionZValue);
+			startTime = Time.time;
+	
+				StartCoroutine (MoveDoor ());
+			
+		}
+			}
+
+	IEnumerator MoveDoor(){
+		while (Time.time < startTime + timeToMove) {
+			doorState = DoorStates.Moving;
+			transform.position = Vector3.Lerp (startPosition, endPosition, (Time.time - startTime) / timeToMove);
+			yield return null;
+
+		}
+		if (wasOpen == true) {
+			doorState = DoorStates.Closed;
+		} else {
+			doorState = DoorStates.Open;
+		}
+	}
+
+		}
+	
+
+
