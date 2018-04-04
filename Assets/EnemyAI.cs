c7f53fcc58514150434a8365d4acd2af867e0b55
Jaimy Magalhaes Coelho <mokin009x@hotmail.nl>
2018-04-04 16:17:57 +0200

    Further development ai
    working on the ai that can run hide and be panicked.

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -7,21 +7,30 @@ public class EnemyAI : MonoBehaviour {
 
 	NavMeshAgent nav;
 	float fov = 120f;
-	Vector3 lastSeen;
+	Vector3 currentHidingSpot;
 
 	public Transform chaser;
 	public Light light;
 	public Mode mode;
 	public float speed;
     public float Sound;
+    public GameObject hideSpots;
     public Transform[] hidingspots;
 
 	// Use this for initialization
-	void Start () {
-		nav = GetComponent<NavMeshAgent>();
-	   
+    private void Awake()
+    {
+
+
+    }
+
+    void Start () {
+
+        nav.destination = hidingspots[Random.Range(0, hidingspots.Length)].transform.position;
+
+        SetMode(Mode.Run);
+
         chaser = GameObject.FindGameObjectWithTag("Player").transform;
-		SetMode(Mode.Patrol);
 		//StartCoroutine(AiBehaviour());
 	}
 	
@@ -31,44 +40,45 @@ public class EnemyAI : MonoBehaviour {
 
 		if(canSee && mode != Mode.Panic)
 		{
-		  
+		  Debug.Log("teste");
 		    
 		        SetMode(Mode.Panic);
 
             
         }
 
+	  
 
         // happens every frame 
-	    switch (mode)
+	    if (canSee&& mode ==Mode.Hide)
 	    {
+	        SetMode(Mode.Run);
+	    }
 
-            case Mode.Run:
-                // a sound comes here
-                break;
-	        case Mode.Panic:
-                // a extra hard sound
-	            break;
-	        case Mode.Hide:
-	            if (canSee == true && mode !=Mode.Panic)
-	            {
-	                
-                    SetMode(Mode.Run);
-	            }
-
-                break;
+	    if (canSee == false)
+	    {
+	        SetMode(Mode.Hide);
         }
+      
+    }
 
-	}
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.CompareTag("HideSpot"))
+        {
+            
 
-	IEnumerator AiBehaviour()
+        }
+    }
+
+    IEnumerator AiBehaviour()
 	{
 		while(true)
 		{
 			if(mode == Mode.Patrol)
 			{
-				Vector3 destination = transform.position + new Vector3(Random.Range(-10, 10), 0f, Random.Range(-10, 10));
-				nav.SetDestination(destination);
+				//Vector3 destination = transform.position + new Vector3(Random.Range(-10, 10), 0f, Random.Range(-10, 10));
+				//nav.SetDestination(destination);
 			}
 			yield return new WaitForSeconds(Random.Range(3,7));
 		}
@@ -83,27 +93,29 @@ public class EnemyAI : MonoBehaviour {
 		switch (mode)
 		{
             case Mode.Run:
+                light.color = Color.yellow;
 
+                nav.speed = 20;
+
+                nav.destination = hidingspots[Random.Range(0,hidingspots.Length)].transform.position;
                 break;
 
             case Mode.Panic:
+                light.color = Color.red;
 
+                nav.speed = 50;
                 break;
             case Mode.Hide:
+                nav.destination = transform.position;
+                light.color = Color.blue;
 
                 break;
 		case Mode.Patrol:
 		
-			light.color = Color.blue;
 			break;
 		case Mode.Search:
-			nav.SetDestination(lastSeen);
-			light.color = Color.yellow;
 			break;
 		case Mode.Chase:
-			light.color = Color.red;
-			lastSeen = chaser.position;
-			CentralIntellignece.instance.Alert(gameObject);
 			break;
 		}
 	}
---
+++ Assets/NavMeshProblem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshProblem : MonoBehaviour
+{
+    private NavMeshAgent nav;
+
+    public Transform hidingspot;
+	// Use this for initialization
+	void Start () {
+	    nav = nav = GetComponent<NavMeshAgent>();
+	    nav.destination = hidingspot.transform.position;
+
+	}
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
