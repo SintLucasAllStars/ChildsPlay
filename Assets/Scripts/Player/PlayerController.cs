5bf483f67503e263f32df682e7180d87c8e78ad7
Bulletdrone <Sam>
2018-04-04 11:48:05 +0200

    PlayerController Script added, used for movement for player. Rotation needs to be added to that script.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 200000;
+
+	Rigidbody rb;
+	Vector3 direction;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		float horizontalMovement = Input.GetAxis ("Horizontal");
+		float verticalMovement = Input.GetAxis ("Vertical");
+
+		direction = (horizontalMovement * transform.right + verticalMovement * transform.forward).normalized;
+	}
+
+	void FixedUpdate () {
+		Movement ();
+	}
+	void Movement () {
+		rb.velocity = direction * speed * Time.deltaTime;
+	}
+}
