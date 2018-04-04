a7be2b7fc0f17de61319f548bfdccf74130c6c20
deMachtige <borisramakers99@outlook.com>
2018-04-04 11:38:58 +0200

    fork has propper hitbox

--- Fork and pull/Assets/StabBehaviour.cs
+++ Fork and pull/Assets/StabBehaviour.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StabBehavure : MonoBehaviour {
+public class StabBehaviour : MonoBehaviour {
 	public float stabSpeed;
 	Transform stabGoal;
 	Transform stabStart;
 	float stabReady = 0f;
 	bool pullingBack = false;
+	BoxCollider coll;
 
 	
 	// Use this for initialization
 	void Start () {
+		coll = gameObject.GetComponent<BoxCollider> ();
+		coll.enabled = false;
 		stabGoal = GameObject.Find("/player/Main Camera/ForkGoal").transform;
 		stabStart = GameObject.Find("/player/Main Camera/ForkStart").transform;
 	}
@@ -21,10 +24,12 @@ public class StabBehavure : MonoBehaviour {
 		if (stabReady < Time.time &&Input.GetMouseButtonDown (0)) {
 			stabReady = Time.time + 0.5f;
 			pullingBack = false;
+			coll.enabled = true;
 		}
 
 		if (transform.localPosition.z >= stabGoal.localPosition.z-0.00001f) {
 			pullingBack = true;
+			coll.enabled = false;
 		}
 			
 		if (!pullingBack) {
