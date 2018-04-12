617d2f70693cf30c062ce336be388a383f49c442
Jesper Seipenbusch <jesper.s.js@gmail.com>
2018-04-12 21:19:18 +0200

    added Custom map with navmesh and started on hide mechanic

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour {
 
+	float range;
+	Vector3 target;
 	/*
 	speed
 	stamina
@@ -14,12 +16,39 @@ public class PlayerManager : MonoBehaviour {
 
 	public enum State {idle, Walk, Run, Exhausted}
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		range = 12f;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		if(Input.GetKey(KeyCode.E))
+		{
+			Debug.Log("pressed E");
+			CheckForBush();
+		}
 		
+	}
+
+	void CheckForBush()
+	{
+		RaycastHit hit;
+
+		if (Physics.Raycast(transform.position, transform.forward, out hit, range))
+		{
+			Debug.Log(hit.transform.tag);
+			if (hit.transform.tag == "Hidingspot")
+			{
+				target = hit.transform.position;
+				Hide();
+			}
+		}
+	}
+
+	void Hide()
+	{
+		transform.position = target;
 	}
 }
