2ac37a8ccc2e943154b97afa6457a40572f603eb
mikedriessen04@gmail.com <36692429+mikedriessen@users.noreply.github.com>
2018-04-14 21:26:43 +0200

    Added player and camera

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
 
+
+	public float speedH = 2.0f;
+	public float speedV = 2.0f;
+
+	private float yaw = 0.0f;
+	private float pitch = 0.0f;
+
+
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+		Cursor.visible = false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	// Update is called once per frame\
+	void Update()
+	{
+
+		yaw += speedH * Input.GetAxis("Mouse X");
+		pitch -= speedV * Input.GetAxis("Mouse Y");
+
+		transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
 	}
 }
--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -1,16 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Win : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Player")){
+		SceneManager.LoadScene(1);
+		Debug.Log("Loaded Scene 1");
+		}
 	}
 }
