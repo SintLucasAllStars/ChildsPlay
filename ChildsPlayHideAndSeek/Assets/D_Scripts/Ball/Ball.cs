ac480dd176c19cd4d5289b328a732aa91afe18f2
Unknown <d.jacobs@sintlucasedu.nl>
2019-03-12 16:34:28 +0100

    Added: - Character Animation. - Character rotation follows mouse. - Almost done making point to walk to.

---
+++ ChildsPlayHideAndSeek/Assets/D_Scripts/Ball/Ball.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    #region Ball stuff
+    public Rigidbody ball_Rb;
+    float speed = 15;
+    #endregion
+    private void Start()
+    {
+        ball_Rb.AddForce(transform.forward * speed, ForceMode.Impulse);
+    }
+}
---
+++ ChildsPlayHideAndSeek/Assets/D_Scripts/MouseLooking/MouseFollower.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollower : MonoBehaviour
+{
+    bool active = true;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            active = !active;
+        }
+
+        if (active == true)
+        {
+            Follow();
+        } else { }
+    }
+
+    void Follow()
+    {
+        Vector3 temp = Input.mousePosition;
+        temp.z = 23.5f; // Set this to be the distance you want the object to be placed in front of the camera.
+        this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+    }
+}
