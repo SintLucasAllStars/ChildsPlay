dac43fdddaa111f1815525b1a8d1e20f7276528b
Gordon <aardappel156@gmail.com>
2019-03-15 11:44:19 +0100

    WIP Equip weapon

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public bool isDead;
+    public GameObject gun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActivateGun(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collider other)
+    {
+        if (other.CompareTag("Gun"))
+        {
+            ActivateGun(true);
+            Destroy(other);
+        }
+    }
+
+
+    public void ActivateGun(bool b)
+    {
+        gun.SetActive(b);
+    }
+}
