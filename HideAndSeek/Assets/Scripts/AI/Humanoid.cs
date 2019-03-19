892c389777fdd0978bc9e87c42919c917ef6e6c6
Quinn McGregor <quinn1115@users.noreply.github.com>
2019-03-19 16:10:42 +0100

    Ad en Quinn's Base Humanoid

---
+++ HideAndSeek/Assets/Scripts/AI/Humanoid.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Humanoid : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+    public float speed;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
+    }
+
+    protected void MoveTo(Vector3 Pos)
+    {
+        agent.destination = Pos;
+    }
+
+
+}
