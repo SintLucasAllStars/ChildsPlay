d9ae6189365a108cd86ff12c811b1c86dc18e29e
JeroenKoolen <48135261+JeroenKoolen@users.noreply.github.com>
2019-03-20 16:04:19 +0100

    Simple pathfinding

--- Assets/Scripts/Children.cs
+++ Assets/Scripts/Children.cs
@@ -6,32 +6,53 @@ using UnityEngine.AI;
 public class Children : MonoBehaviour
 {
     public NavMeshAgent agent;
-    private float distance;
+    public float distance;
     private float posX;
     private float posZ;
     Vector3 Rlocation;
     GameObject Player;
+    Vector3 posPlayer;
+    Vector3 newLocation;
 
 
         void Start()
     {
+        //palt de navmeshagent voor movement en maakt een nieuwe random locatie aan om naar toe tegaan
         agent = gameObject.GetComponent<NavMeshAgent>();
         Rlocation = new Vector3(posX, -3.87f, posZ);
-        Player = find
+        //vind de player 
+        Player = GameObject.FindGameObjectWithTag("Player");
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //pakt de player transform
+        posPlayer = Player.GetComponent<Transform>().position;
+        //loopt naar de nieuwe positie
         agent.SetDestination(Rlocation);
-      
+        //berekent hoe ver de player van he kind vandaan is 
+        distance = Vector3.Distance(transform.position, posPlayer);
+        if(distance < 3)
+        {
+            ChangePosCoords();
+            Rlocation = new Vector3(posX, -3.87f, posZ);
+            Debug.Log("WEEWOO");
+            return;
 
-        if ())
+        }
+
+
+
+        //debug .log if Random pos
+        if (Input.GetKeyDown(KeyCode.Space)) 
         {
             ChangePosCoords();
             Rlocation = new Vector3(posX, -3.87f, posZ);
         }
     }
+    // nieuwe positie aanmaken
     void ChangePosCoords()
     {
         posX = Random.Range(-4, 4);
