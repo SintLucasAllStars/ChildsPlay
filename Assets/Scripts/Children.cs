5e8870875d0fe73bd816cd562033b61fe14bcb8b
JeroenKoolen <48135261+JeroenKoolen@users.noreply.github.com>
2019-03-22 10:24:08 +0100

    ai script

--- Assets/Scripts/Children.cs
+++ Assets/Scripts/Children.cs
@@ -9,14 +9,24 @@ public class Children : MonoBehaviour
     public float distance;
     private float posX;
     private float posZ;
+    bool changeLocation;
     Vector3 Rlocation;
     GameObject Player;
     Vector3 posPlayer;
     Vector3 newLocation;
+   
+
+
+
+
+    public float multiplyBy;
 
 
         void Start()
     {
+        changeLocation = false;
+        multiplyBy = 0.2f;
+      
         //palt de navmeshagent voor movement en maakt een nieuwe random locatie aan om naar toe tegaan
         agent = gameObject.GetComponent<NavMeshAgent>();
         Rlocation = new Vector3(posX, -3.87f, posZ);
@@ -31,31 +41,53 @@ public class Children : MonoBehaviour
         //pakt de player transform
         posPlayer = Player.GetComponent<Transform>().position;
         //loopt naar de nieuwe positie
-        agent.SetDestination(Rlocation);
+      //  agent.SetDestination(Rlocation);
         //berekent hoe ver de player van he kind vandaan is 
         distance = Vector3.Distance(transform.position, posPlayer);
-        if(distance < 3)
+        if(distance < 5)
         {
-            ChangePosCoords();
-            Rlocation = new Vector3(posX, -3.87f, posZ);
-            Debug.Log("WEEWOO");
-            return;
+                      
+            move();
+            Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+            Debug.DrawRay(transform.position, forward, Color.green);
+            agent.Stop();
+            changeLocation = true;
+            if (changeLocation)
+            {
+                posX = Random.Range(-4, 4);
+                posZ = Random.Range(-4, 4);
+                Rlocation = new Vector3(posX, -3.87f, posZ);
+                changeLocation = false;
+            }
+
+
 
         }
+        else
+        {
+     
+            agent.SetDestination(Rlocation);
+            agent.Resume();
 
+        }
+       
 
 
         //debug .log if Random pos
         if (Input.GetKeyDown(KeyCode.Space)) 
         {
-            ChangePosCoords();
+           
             Rlocation = new Vector3(posX, -3.87f, posZ);
         }
     }
     // nieuwe positie aanmaken
-    void ChangePosCoords()
+    void move()
     {
-        posX = Random.Range(-4, 4);
-        posZ = Random.Range(-4, 4);
+        transform.rotation = Quaternion.LookRotation(transform.position - posPlayer).normalized;
+        transform.Translate(0, 0, 0.02f);
+       // Vector3 RunTo = transform.position + transform.forward * multiplyBy;
     }
+   
+    
+   
 }
---
+++ Assets/Scripts/Simplemovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Simplemovement : MonoBehaviour
+{
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 4f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal")* speed * Time.deltaTime, 0, Input.GetAxis("Vertical")* speed * Time.deltaTime);
+
+
+        
+    }
+}
