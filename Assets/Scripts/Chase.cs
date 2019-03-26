cbcf41a8c7ff8fd61dc25820f18114b30c312e8e
Vincent <47562108+VeCystim@users.noreply.github.com>
2019-03-26 14:28:50 +0100

    Initial commit

---
+++ Assets/Scripts/Chase.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Chase : MonoBehaviour
+{
+
+
+	bool tagged = false;
+
+	[SerializeField]
+	Transform destination;
+
+	NavMeshAgent navMeshAgent;
+
+	// Use this for initialization
+	void Start()
+	{
+
+		navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+
+	}
+
+	private void Update()
+	{
+		if (navMeshAgent == null)
+		{
+			//Debug.LogError("The nav mesh agent component is not attached to " + gameObject.name);
+		}
+		else
+		{
+			SetDestination();
+		}
+	}
+
+	private void SetDestination()
+	{
+		if (destination != null)
+		{
+			Vector3 targetVector = destination.transform.position;
+			if (tagged == true)
+			{
+				navMeshAgent.SetDestination(-targetVector);
+			}
+			else
+			{
+				navMeshAgent.SetDestination(targetVector);
+			}
+
+		}
+		
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "player")
+		{
+			Debug.Log("Kanker");
+			tagged = true;
+			
+		}
+	}
+}
---
+++ Assets/Scripts/ChaseMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChaseMove : MonoBehaviour {
+
+	[SerializeField]
+	Transform destination;
+
+	NavMeshAgent navMeshAgent;
+
+	// Use this for initialization
+	void Start() {
+
+		navMeshAgent = this.GetComponent<NavMeshAgent>();
+		
+
+	}
+
+	private void Update()
+	{
+		if (navMeshAgent == null)
+		{
+			//Debug.LogError("The nav mesh agent component is not attached to " + gameObject.name);
+		}
+		else
+		{
+			SetDestination();
+		}
+	}
+
+	private void SetDestination()
+	{
+		if(destination != null)
+		{
+			Vector3 targetVector = destination.transform.position;
+			navMeshAgent.SetDestination(targetVector);
+		}
+	}
+}
