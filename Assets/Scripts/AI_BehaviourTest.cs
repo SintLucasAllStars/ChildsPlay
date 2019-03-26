457959618054c744dec9fa17a0559860b7814e67
Kevin Li <Gwnk3vin@gmail.com>
2019-03-26 14:30:48 +0100

    Map pushed
    - Lighting is still missing although it looks decend.

--- Assets/Scripts/AI_BehaviourTest.cs
+++ Assets/Scripts/AI_BehaviourTest.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class AI_BehaviourTest : MonoBehaviour
 {
+
+	public int Size;
+	public float Offset; 
+	public GameObject m_Obj; 
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+		for (int z = 0; z < Size; z++)
+		{
+			for (int x = 0; x < Size; x++)
+			{
+				Instantiate(m_Obj, new Vector3(x, 0, z), Quaternion.identity);
+
+			}
+		}
     }
 
     // Update is called once per frame
