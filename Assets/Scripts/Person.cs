a646d6531da13465a88b0bd86f4e8ef3b8d2519b
Gordon <aardappel156@gmail.com>
2019-03-29 11:51:07 +0100

    Start baseclass for player and enemy

---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+
+    public GameObject droppedgun;
+    public GameObject gunDropPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DropWeapon()
+    {
+        Debug.Log("Dropped weapon");
+        droppedgun = Instantiate(gunDropPrefab, shootOffset.position, transform.rotation);
+
+        Rigidbody rb = droppedgun.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * 1000);
+
+        //calling ActivateDropWeapon
+        //Invoke("ActivateDropWeapon", 0.5f);
+
+        gun.SetActive(false);
+        hasShot = false;
+    }
+
+    public void RecieveWeapon()
+    {
+        Debug.Log(gameObject.name + "got weapon");
+        //get weapon
+    }
+}
