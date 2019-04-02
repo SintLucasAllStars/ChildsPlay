f9cc6a01ae176a3be5f25dd708bd809c6a681c33
JeroenKoolen <48135261+JeroenKoolen@users.noreply.github.com>
2019-04-02 11:47:22 +0200

    Revert "Revert "Version 2.6""
    This reverts commit 859f5f06cee2091c22773577d711bf563db8c94c.

--- Assets/Scripts/ChildManager.cs
+++ Assets/Scripts/ChildManager.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class ChildManager : MonoBehaviour
 {
-    //list van de kinderen
+    //list of the customers
     private List<GameObject> m_Children = new List<GameObject>();
 
-    // public GameObject m_prefab;
-    // public GameObject m_KermitPrefab;
     public GameObject[] m_customers;
 
     private Vector3 m_randomVector;
@@ -22,6 +20,8 @@ public class ChildManager : MonoBehaviour
 
         SpawnKinderen();
 
+        SpawnCitizens();
+
     }
 
   
@@ -32,16 +32,16 @@ public class ChildManager : MonoBehaviour
 
     }
 
-    //houd de kinderen aantall
+    // Holds the customer amount
     public void ChildrenCount() {
 
         count = m_Children.Count;
         Debug.Log(count);
 
     }
+
     private void SpawnKinderen()
     {
-
         for (int i = 0; i < count; i++) // Repeat this equal to the amount of customers you want
         {
 
@@ -52,84 +52,10 @@ public class ChildManager : MonoBehaviour
             m_Children.Add(g); // Add to array
 
         }
-
-        /*
-        for (int i = 0; i < 5; i++)
-        {
-
-            m_randomVector = new Vector3(Random.Range(-70, 50), 1, Random.Range(-50, 60));
-            GameObject g = Instantiate(m_prefab, m_randomVector, transform.rotation);
-            m_Children.Add(g);
-
-        }
-        for (int i = 0; i < 7; i++)
-        {
-
-            m_randomVector = new Vector3(Random.Range(-70, 50), 1, Random.Range(-50, 60));
-            GameObject g = Instantiate(m_KermitPrefab, m_randomVector, transform.rotation);
-            m_Children.Add(g);
-
-        }
-        */
-
     }
-    /*
-    public void IncreaseSpeed()
+
+    private void SpawnCitizens()
     {
-        int happend = 0;
-        happend += 1;
-        switch (happend)
-        {
-            case 0:
-                foreach (GameObject G in m_Children)
-                {
-
-                    G.GetComponent<Children>().m_speed = 0.03f;
-                }
-                break;
-            case 1:
-                foreach (GameObject G in m_Children)
-                {
-
-                    G.GetComponent<Children>().m_speed = 0.04f;
-                }
-                break;
-            case 2:
-                foreach (GameObject G in m_Children)
-                {
-
-                    G.GetComponent<Children>().m_speed = 0.05f;
-                }
-                break;
-            case 3:
-                foreach (GameObject G in m_Children)
-                {
-
-                    G.GetComponent<Children>().m_speed = 0.06f;
-                }
-                break;
-            case 4:
-                foreach (GameObject G in m_Children)
-                {
-
-                    G.GetComponent<Children>().m_speed = 0.07f;
-                }
-                break;
-            default:
-                foreach (GameObject G in m_Children)
-                {
-
-                    G.GetComponent<Children>().m_speed = 0.04f;
-                }
-                break;
-        }
-      
 
     }
-    */
-
-
-
-
-
 }
