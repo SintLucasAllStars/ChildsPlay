4669ce3268251c8f0aaf527d00cd32beda59b124
Quinn McGregor <quinn1115@users.noreply.github.com>
2019-04-02 13:24:04 +0200

    Added Base Hider AI
    - Base Hider AI

---
+++ HideAndSeek/Assets/Scripts/AI/Hider.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hider : Humanoid
+{
+    List<EnvQueryItem> hideLocations = new List<EnvQueryItem>();
+    public EnvQuerySystem EQSSystem;
+
+    GameObject Seeker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       Seeker = GameObject.FindGameObjectWithTag("Seeker");
+        FindNewLocation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            FindNewLocation();
+        }
+        LookForSeeker();
+    }
+
+    void FindNewLocation()
+    {
+        foreach (EnvQueryItem item in EQSSystem.QueryItems)
+        {
+            if (item.EnemyNearby == false && item.IsColliding == false)
+            {
+                hideLocations.Add(item);
+            }
+        }
+        MoveTo(hideLocations[Random.Range(0, hideLocations.Count)].GetWorldLocation());
+    }
+
+    void LookForSeeker()
+    {
+        bool seekerSeen = false;
+        Ray ray = new Ray(transform.position + new Vector3(0,1,0), (Seeker.transform.position - transform.position) );
+        if (seekerSeen == false)
+        {
+            if (Physics.Raycast(ray, out RaycastHit hit, 10))
+            {
+                if (hit.collider.CompareTag("Seeker"))
+                {
+                    seekerSeen = true;
+                    FindNewLocation();
+                }
+                else
+                {
+                    seekerSeen = false;
+                }
+            }
+        }
+    }
+
+}
+
