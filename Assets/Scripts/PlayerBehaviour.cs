2faa12a706c5e163daf6df42f522adcd1b9e2f5e
Azathoth <m.sluijtman@sintlucasedu.nl>
2020-03-19 11:52:22 +0100

    Added basic player movement

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private Camera cam;
+
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        cam = FindObjectOfType<Camera>();
+        agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
