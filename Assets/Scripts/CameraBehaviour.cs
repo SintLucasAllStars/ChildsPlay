94e17736ada6b134d7293d2d83669aa9323ce852
Azathoth <m.sluijtman@sintlucasedu.nl>
2020-03-19 12:10:29 +0100

    Added baiss for camera zooming

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class CameraBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Camera camera;
+    private float maxFov = 80;
+    private float minFov = 40;
     void Start()
     {
-        
+        camera = FindObjectOfType<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Failed attempt at locking the FOV
+        Mathf.Clamp(camera.fieldOfView, minFov, maxFov);
+        
+        if (Input.GetKey(KeyCode.KeypadMinus))
+        {
+            camera.fieldOfView++;
+        }
+
+        if (Input.GetKey(KeyCode.KeypadPlus))
+        {
+            camera.fieldOfView--;
+        }
         
     }
 }
