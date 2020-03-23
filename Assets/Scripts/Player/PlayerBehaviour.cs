0dfe0b43da064d6ff006418f2b8c454eb9abcc52
BoLadan <60391973+BoLadan@users.noreply.github.com>
2020-03-23 17:00:47 +0100

    PlayerBehaviour script
    In the folder Scripts-Player i made a Player behaviour script
    The player moves with transfom.Translate. The input is regestrated with Input.GetAxis

---
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.Translate(h, 0, z);
+    }
+}
