0d7ac2cc1ae7c74efddaea15188c9bd8c4d7d7af
BoLadan <60391973+BoLadan@users.noreply.github.com>
2020-03-23 17:17:16 +0100

    basic NPC
    added a NavMeshAgent and made it so that the Enemy will always follow the player. This is done in the EnemyBehaviour scipt

---
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; //needed to word with the AI components
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    NavMeshAgent nav; //NavMeshAgent (nav) on enemy
+
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.destination = target.position;
+    }
+}
