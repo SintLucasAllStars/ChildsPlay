0725cd76416ebaec24ce7c391ec37d531977c2f5
MaxStruijs <maxstruys32@gmail.com>
2020-03-24 16:23:20 +0100

    Changed a bit in the Start menu

--- Assets/Scenes/MainMenu/Scripts/MenuBehaviour.cs
+++ Assets/Scenes/MainMenu/Scripts/MenuBehaviour.cs
@@ -7,11 +7,13 @@ public class MenuBehaviour : MonoBehaviour
 {
     public void OnClickedStart() //loads scene with the Index "0-1-2-3-4"
     {
+        print("start is pressed");
         SceneManager.LoadScene("House");
     }
 
     public void OnClickedQuit() //Quit the application
     {
+        print("quit is pressed");
         Application.Quit();
     }
 }
