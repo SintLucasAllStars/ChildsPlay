0df1a61f5e88e21b7e45c8851b3707536796615f
Jarno Looij <jarnolooij@gmail.com>
2020-03-24 14:16:11 +0100

    added full movement
    added full movement to the plyerBehaviour

--- Assets/Scripts/playerBehaviour.cs
+++ Assets/Scripts/playerBehaviour.cs
@@ -17,5 +17,19 @@ public class playerBehaviour : MonoBehaviour
         {
             transform.Translate(0, 0, 0.01f);
         }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -0.01f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-0.01f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0.01f, 0, 0);
+
+        }
+
     }
 }
