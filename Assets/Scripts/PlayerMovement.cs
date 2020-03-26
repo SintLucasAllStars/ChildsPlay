5328ab120cec35cbfe054087f3bbcefc34f9fa65
LuukHendriks99 <luukhendriks2@gmail.com>
2020-03-26 13:59:15 +0100

    PlayerMovement.cs
    Added:
    -region player input

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //player object
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,23 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        #region player input
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.position += new Vector3(0, 0, -1) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            player.transform.position += new Vector3(-1, 0, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.position += new Vector3(0, 0, 1) * Time.deltaTime;
+        }
+        #endregion
     }
 }
