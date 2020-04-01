e9a764325a03170881f598bf52fa253eef89753b
ArcusWithak <56251011+ArcusWithak@users.noreply.github.com>
2020-04-01 23:06:50 +0200

    added sprinting and crouching mechanic

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -14,6 +14,10 @@ public class PlayerBehaviour : MonoBehaviour
 
     private RaycastHit hit;
 
+    private bool crounching;
+    GameObject sound = null;
+
+
     private void Start()
     {
         cam = FindObjectOfType<Camera>();
@@ -37,9 +41,65 @@ public class PlayerBehaviour : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit))
             {
-               GameObject sound = Instantiate(soundPing, hit.point, Quaternion.identity);
+                sound = Instantiate(soundPing, hit.point, Quaternion.identity);
                 Destroy(sound, 0.1f);
             }
         }
+
+        #region
+        //crouching
+        #region crouching
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            if (crounching)
+            {
+                crounching = false;
+
+                transform.position = new Vector3(transform.position.x, transform.position.y + 0.6f, transform.position.z);
+                transform.localScale = new Vector3(1, 1, 1);
+
+                agent.speed *= 2;
+                agent.acceleration *= 2;
+                agent.angularSpeed *= 2;
+            }
+            else
+            {
+                crounching = true;
+
+                transform.localScale = new Vector3(1, 0.5f, 1);
+                transform.position = new Vector3(transform.position.x, transform.position.y - 0.4f, transform.position.z);
+
+                agent.speed /= 2;
+                agent.acceleration /= 2;
+                agent.angularSpeed /= 2;
+            }
+        }
+        #endregion
+        #region sprint
+        if (!crounching)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                agent.speed *= 2;
+                agent.acceleration *= 2;
+                agent.angularSpeed *= 2;
+            }
+            else if (Input.GetKey(KeyCode.LeftShift))
+            {
+                if (sound == null)
+                {
+                    sound = Instantiate(soundPing, transform.position, Quaternion.identity);
+                    Destroy(sound, 0.1f);
+                }
+            }
+            else if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                agent.speed /= 2;
+                agent.acceleration /= 2;
+                agent.angularSpeed /= 2;
+            }
+        }
+        #endregion
+        #endregion
     }
 }
