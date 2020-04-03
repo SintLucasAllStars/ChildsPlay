7f0da0329eeb1c79099ee84df0f5154503655a4b
Steffew <xgenyofficial@gmail.com>
2020-04-03 20:09:51 +0200

    Added gameStarted
    A bool that tells the seeker to start seeking for its prey.
    
    - Renamed seeker prefab and added a spotlight.
    - Edited lighting.

--- Assets/EnemyBehaviour.cs
+++ Assets/EnemyBehaviour.cs
@@ -12,9 +12,10 @@ public class EnemyBehaviour : MonoBehaviour
 
     public Transform target;
     NavMeshAgent nvmesh;
-    public float fov = 60;
+    public float fov = 60; 
     public float range = 3;
     public States currentState = States.Patrol;
+    public bool gameStarted = false;
 
     void Start()
     {
@@ -24,48 +25,56 @@ public class EnemyBehaviour : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {  
-        if (SeesTarget())
+    {
+        if (Input.GetKey(KeyCode.Space))
         {
-            currentState = States.Chase;
-        }
-        else
-        {
-            currentState = States.Patrol;
+            gameStarted = true;
         }
 
-        switch (currentState)
+        if (gameStarted)
         {
-            case States.Patrol:
-                if (Time.frameCount % 60 == 0)
-                {
-                    Vector3 newDest = transform.position + new Vector3(Random.Range(-1f, 1f), transform.position.y, Random.Range(-1f, 1f)) *6;
-                    nvmesh.destination = newDest;
-                }
-                break;
-            case States.Chase:
-                nvmesh.destination = target.position;
-                break;
+            if (SeesTarget())
+            {
+                currentState = States.Chase;
+            }
+            else
+            {
+                currentState = States.Patrol;
+            }
+
+            switch (currentState)
+            {
+                case States.Patrol:
+                    if (Time.frameCount % 30 == 0)
+                    {
+                        Vector3 newDest = transform.position + new Vector3(Random.Range(-1f, 1f), transform.position.y, Random.Range(-1f, 1f)) * 6;
+                        nvmesh.destination = newDest;
+                    }
+                    break;
+                case States.Chase:
+                    nvmesh.destination = target.position;
+                    break;
+            }
         }
     }
 
     public bool SeesTarget()
-    {
-        Vector3 p = target.position - transform.position;
-        float angle = Vector3.Angle(p, transform.forward);
-        if (angle <= fov / 2.0f)
         {
-            RaycastHit hit;
-
-            if (Physics.Raycast(origin: transform.position, direction: p, out hit, range))
+            Vector3 p = target.position - transform.position;
+            float angle = Vector3.Angle(p, transform.forward);
+            if (angle <= fov / 2.0f)
             {
-                if (hit.collider.gameObject.CompareTag("Player"))
+                RaycastHit hit;
+
+                if (Physics.Raycast(origin: transform.position, direction: p, out hit, range))
                 {
-                    return true;
+                    if (hit.collider.gameObject.CompareTag("Player"))
+                    {
+                        return true;
+                    }
                 }
             }
-        }
 
-        return false;
-    }
+            return false;
+        }
 }
