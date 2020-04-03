2576d85b8a833f7a35ed6e49ff0d6b8bf575949c
Steffew <xgenyofficial@gmail.com>
2020-04-03 19:37:51 +0200

    Initial commit
    Added assets from previous projects to use as a template.
    
    Co-Authored-By: qsib <qsib@users.noreply.github.com>

---
+++ Assets/EnemyBehaviour.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public enum States
+    {
+        Patrol, Chase
+    }
+
+    public Transform target;
+    NavMeshAgent nvmesh;
+    public float fov = 60;
+    public float range = 3;
+    public States currentState = States.Patrol;
+
+    void Start()
+    {
+        Debug.Log(target.position);
+        nvmesh = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        if (SeesTarget())
+        {
+            currentState = States.Chase;
+        }
+        else
+        {
+            currentState = States.Patrol;
+        }
+
+        switch (currentState)
+        {
+            case States.Patrol:
+                if (Time.frameCount % 60 == 0)
+                {
+                    Vector3 newDest = transform.position + new Vector3(Random.Range(-1f, 1f), transform.position.y, Random.Range(-1f, 1f)) *6;
+                    nvmesh.destination = newDest;
+                }
+                break;
+            case States.Chase:
+                nvmesh.destination = target.position;
+                break;
+        }
+    }
+
+    public bool SeesTarget()
+    {
+        Vector3 p = target.position - transform.position;
+        float angle = Vector3.Angle(p, transform.forward);
+        if (angle <= fov / 2.0f)
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(origin: transform.position, direction: p, out hit, range))
+            {
+                if (hit.collider.gameObject.CompareTag("Player"))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Assets/PlayerBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float speed = 0.05f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed, 0, 0);
+        }
+    }
+}
