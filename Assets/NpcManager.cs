e7644e6a16e2f2752e75ea0723bf17fb08a64dd3
Shan <shanoomes@gmail.com>
2020-04-07 11:32:09 +0200

    Added Npc's to house scene

--- Assets/NpcManager.cs
+++ Assets/NpcManager.cs
@@ -6,6 +6,7 @@ public class NpcManager : MonoBehaviour
 {
     public static NpcManager Instance { get; set; }
     public GameObject enemy;
+    public Transform[] spawnPoints;
     private void Awake()
     {
         if (Instance == null)
@@ -22,10 +23,11 @@ public class NpcManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 8; i++)
         {
-            GameObject obj = Instantiate(enemy, new Vector3(i * 2.0f, 0, 0), Quaternion.identity);
-            //GetComponent<Renderer>().material.color = new Color(0, 255, 0);
+            int randomSpawnPoint = Random.Range(0, spawnPoints.Length);
+            GameObject obj = Instantiate(enemy, spawnPoints[randomSpawnPoint].position, Quaternion.identity);
+
             obj.transform.parent = this.gameObject.transform;
         }
 
