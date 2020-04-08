bb9d45ab811ca76ff90c00226eee25471bc7c83c
Vlams <flvlams@gmail.com>
2020-04-08 21:56:35 +0200

    Added CameraFollow script
    Added CameraFollow script with target parameter

---
+++ NavMesh/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform target;
+
+    private Vector3 offset;
+    
+    void Start() {
+        offset = transform.position - target.position;
+    }
+    
+    void Update() {
+        transform.position = target.position + offset;
+    }
+}
