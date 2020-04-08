55630753316fc6ac6a7912401380da77687ca6de
Vlams <flvlams@gmail.com>
2020-04-08 18:46:44 +0200

    Created PlayerController script
    Created player controller with speed and acceleration controls.

---
+++ NavMesh/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 10;
+    public float acceleration = 20;
+
+    private CharacterController pc;
+    private Vector3 velocity;
+
+    private void Start() {
+        pc = GetComponent<CharacterController>();
+    }
+
+    void Update() {
+        Vector3 dir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        velocity -= velocity.normalized * Mathf.Min(velocity.magnitude, acceleration * Time.deltaTime);
+        velocity += dir.normalized * acceleration * 3 * Time.deltaTime;
+        if (velocity.magnitude > speed) velocity = velocity.normalized * speed;
+        pc.Move(velocity * Time.deltaTime);
+    }
+}
